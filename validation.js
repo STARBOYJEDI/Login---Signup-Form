7dc445ab2dd3cea16d60057384ca2e23fa38841f
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-03-10 19:09:57 +0200

    - Created an eventListener function that will handle login logic for the firstname input value, and error handling for when the firstname has no input value from the user

--- validation.js
+++ validation.js
@@ -4,7 +4,20 @@ const email_input = document.getElementById('email-input');
 const password_input = document.getElementById('password-input');
 const repeat_password_input = document.getElementById('repeat-password-input');
 
+form.addEventListener('submit', (e) => {
+    // e.preventDefault() Prevent Submit
 
+    let errors = [];
+
+    if (firstname_input) {
+        // If we have a firstname input then we are in the signup
+        errors = getSignupFormErrors();
+    }
+    else {
+        // If we don't have a firstname input then we are in the login
+        errors = getLoginFormErrors();
+    }
+})
 
 
 
