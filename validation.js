26a5333897ff1ba97ca8aa9d4cb2e15b718fc746
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-03-10 19:34:14 +0200

    - Minor fix to html link to javascript file - Added an if statement in the eventListener function that handle logic for when the user tries to submit the form without adding any details to the signup form.

--- validation.js
+++ validation.js
@@ -1,41 +1,46 @@
-const form = document.getElementById('form');
-const firstname_input = document.getElementById('firstname-input');
-const email_input = document.getElementById('email-input');
-const password_input = document.getElementById('password-input');
-const repeat_password_input = document.getElementById('repeat-password-input');
+const form = document.getElementById('form')
+const firstname_input = document.getElementById('firstname-input')
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat-password-input')
 
 form.addEventListener('submit', (e) => {
     // e.preventDefault() Prevent Submit
 
-    let errors = [];
+    let errors = []
 
     if (firstname_input) {
         // If we have a firstname input then we are in the signup
-        errors = getSignupFormErrors(firstname_input.ariaValueMax, email_input.ariaValueMax, password_input.value, repeat_password_input.value);
+        errors = getSignupFormErrors(firstname_input.ariaValueMax, email_input.ariaValueMax, password_input.value, repeat_password_input.value)
     }
     else {
         // If we don't have a firstname input then we are in the login
-        errors = getLoginFormErrors(email_input.value, password_input.value);
+        errors = getLoginFormErrors(email_input.value, password_input.value)
+    }
+
+    if (errors.length > 0) {
+        // If there are any errors
+        e.preventDefault()
     }
 });
 
 function getSignupFormErrors(firstname, email, password, repeatPassword) {
-    let errors = [];
+    let errors = []
 
     if (firstname === '' || firstname == null) {
-        errors.push('Firstname is required');
-        firstname_input.parentElement.classList.add('incorrect');
+        errors.push('Firstname is required')
+        firstname_input.parentElement.classList.add('incorrect')
     }
     if (email === '' || email == null) {
-        errors.push('Email is required');
-        email_input.parentElement.classList.add('incorrect');
+        errors.push('Email is required')
+        email_input.parentElement.classList.add('incorrect')
     }
     if (password === '' || password == null) {
         errors.push('Password is required');
-        password_input.parentElement.classList.add('incorrect');
+        password_input.parentElement.classList.add('incorrect')
     }
 
-    return errors;
+    return errors
 }
 
 
