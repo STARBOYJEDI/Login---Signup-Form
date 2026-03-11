a3948ed7eb1a20fdb6c8eaeac4dff1200efc9569
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-03-11 18:20:18 +0200

    - Added an if statement that will display an error message whenever the user inputs a password that is not longer than 8 characters

--- validation.js
+++ validation.js
@@ -41,6 +41,10 @@ function getSignupFormErrors(firstname, email, password, repeatPassword) {
         errors.push('Password is required');
         password_input.parentElement.classList.add('incorrect');
     }
+    if (password.length < 8) {
+        errors.push('Password must have at least 8 characters');
+        password_input.parentElement.classList.add('incorrect');
+    }
     if (password !== repeatPassword) {
         errors.push('Password does not match repeated password');
         password_input.parentElement.classList.add('incorrect');
